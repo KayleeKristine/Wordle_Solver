40696e5984b754f2742c036c3342aff7d9ba1bd3
kayleet <kayleekthomas@gmail.com>
2023-05-05 19:08:53 -0700

    starting to implement class Board

--- wordle_gboard.py
+++ wordle_gboard.py
@@ -3,18 +3,33 @@
 A Wordle board holds a matrix of tiles.
 
 """
-"""
+
 class Board():
 
     def __init__(self, r_num, c_num):
         # number of rows = number of guesses
         # number of cols = number of letters within valid word
 
-        self.board = [[] for i in range(r_num)]
         self.word_size = c_num
         self.total_guesses = r_num
+        self.board = [[] for i in range(r_num)]
 
-"""
+        for row in range(self.total_guesses):
+            cols = []
+            for col in range(self.word_size):
+                cols.append(Tile(row, col))
+            self.board.append(col)
+        
+    def print_board(self):
+        
+        f'Wordle Game Board\n'
+        for i in range(self.total_guesses):
+            f'{i}'
+            for j in range(self.word_size):
+                f'{j}'
+                f'{self.board[i][j]}'
+            f'\n'
+        
 
 
 class Tile:
@@ -26,7 +41,7 @@ class Tile:
         self.color = color
 
     def __str__(self):
-        return f'Tile: {self.letter} {self.color}'
+        return f'Tile: {self.letter}, {self.color}'
 
     def __repr__(self):
         return f'Tile(letter={self.letter}, color={self.color}, row={self.row}, col={self.com})'
@@ -35,6 +50,11 @@ class Tile:
 def main():
     x = Tile(1, 2, 'f', 'yellow')
     print(x)
+
+    y = Board(3, 4)
+    y.print_board()
+
+
 
 main()
 
