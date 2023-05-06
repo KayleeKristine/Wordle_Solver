85a847a7578da780ba86363a1c9878c0b5a746c8
kayleet <kayleekthomas@gmail.com>
2023-05-05 19:21:14 -0700

    Created global configurations for game board - Need to implement

---
+++ wordle_config.py
@@ -0,0 +1,16 @@
+"""
+Configurations
+
+Control variations of the game Wordle
+"""
+
+# number of guesses
+NGUESSES = 5
+
+# valid word length
+NWORDLEN = 5
+
+# set of symbols that can be used for a tile
+CHOICES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+UNKNOWN = "."
+
