4184ec45c3cbac3966bb1c93c216dcbd34dfa7c9
kayleet <kayleekthomas@gmail.com>
2023-05-05 20:02:25 -0700

    able to print board and see tile letters within

--- wordle_gboard.py
+++ wordle_gboard.py
@@ -13,29 +13,42 @@ from wordle_config import CHOICES, UNKNOWN
 
 class Board():
 
-    def __init__(self, r_num, c_num):
+    def __init__(self):
         # number of rows = number of guesses
         # number of cols = number of letters within valid word
+        self.board = []
+        self.groups = []
 
-        self.word_size = c_num
-        self.total_guesses = r_num
-        self.board = [[] for i in range(r_num)]
-
-        for row in range(self.total_guesses):
+        for row in range(NGUESSES):
             cols = []
-            for col in range(self.word_size):
+            for col in range(NWORDLEN):
                 cols.append(Tile(row, col))
-            self.board.append(col)
-        
+            self.board.append(cols)
+
+
+
     def print_board(self):
         
-        f'Wordle Game Board\n'
-        for i in range(self.total_guesses):
-            f'{i}'
-            for j in range(self.word_size):
-                f'{j}'
-                f'{self.board[i][j]}'
-            f'\n'
+        print(f'*** Wordle Game Board ***')
+        
+        for row in self.board:
+            #letters = [tile.letter for tile in row]
+            letters = ""
+            for tile in row:
+                #print(f'{tile.letter}')
+                letters = letters + tile.letter
+
+            print(f'{letters}')
+            
+
+
+        """
+        for i in range(NGUESSES):
+            
+            for j in range(NWORDLEN):
+                
+                print(f'{self.board[i][j].letter}'.strip())
+        """ 
         
 
 
@@ -48,21 +61,24 @@ class Tile:
         self.color = color
 
     def __str__(self):
-        return f'Tile: {self.letter}, {self.color}'
+        return f'Tile: {self.letter} {self.color}'
 
     def __repr__(self):
-        return f'Tile(letter={self.letter}, color={self.color}, row={self.row}, col={self.com})'
+        return f'Tile(letter={self.letter}, color={self.color}, row={self.row}, col={self.col})'
 
 
 def main():
     # checking if import works
-    print(CHOICES, NGUESSES)
+    #print(CHOICES, NGUESSES)
 
     x = Tile(1, 2, 'f', 'yellow')
     print(x)
-    
 
-    y = Board(3, 4)
+    z = Tile(3, 5)
+    print(z)
+    
+    
+    y = Board()
     y.print_board()
 
 
