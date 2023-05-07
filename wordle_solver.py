6a619a4f35b774b304565ec53a6db402a96e916e
kayleet <kayleekthomas@gmail.com>
2023-05-06 20:44:19 -0700

    outline of wordle_solver

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,2 +1,28 @@
-# testing
+"""
+Main driver
+"""
 
+import wordle_gboard as wgb
+
+def main():
+    # test import
+    b = wgb.Board()
+    b.print_board()
+
+    # pick target word
+    # randomize? for now just assign
+    target = "wants"
+
+    # while remaining guess available or guesses target
+    # ask user for guess
+
+    # check if guess valid
+
+    # add guess to board
+
+    # add corresponding colors
+
+    # print out board
+
+if __name__=="__main__":
+    main()
