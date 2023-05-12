f41cea8ae77803dcd4f6a89125e6d281ad08e6b5
kayleet <kayleekthomas@gmail.com>
2023-05-12 12:48:16 -0700

    made comments specific to solver

--- wordle_solver.py
+++ wordle_solver.py
@@ -3,24 +3,26 @@ Main driver
 """
 
 import wordle_gboard as wgb
+from wordle_config import NWORDLEN
 
 def main():
     # test import
     b = wgb.Board()
-    b.print_board()
+    #b.print_board()
 
     # pick target word
     # randomize? for now just assign
     target = "wants"
 
     # while remaining guess available or guesses target
-    # ask user for guess
+    # choose guess from dictionary
 
-    # check if guess valid
 
     # add guess to board
 
+
     # add corresponding colors
+
 
     # print out board
 
