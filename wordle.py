f135dc754919fc8ecca2b32b4a3d9d14f6f0c208
kayleet <kayleekthomas@gmail.com>
2023-05-29 17:11:30 -0700

    wordle.py now uses update_board from class Board

--- wordle.py
+++ wordle.py
@@ -50,68 +50,8 @@ def main():
             
 
         # add guess to board
+        b.update_board(guess, counter)
         
-        # find corresponding colors
-        for i in range(NWORDLEN):
-            #print(f'{guess[i]}')
-            glet = guess[i]
-            print(f'\nProcessing - {glet} - position: {i}')
-
-            for j in range(NWORDLEN):
-                #print(f'{target[j]}')
-                print(f'comparing with - {TARGET[j]} - position: {j}')
-                
-                # if letter in correct position and within target
-                if i == j and glet is TARGET[j]:
-                    print(f'letter and position match')
-
-                    # color green
-                    b.greens[i].append(glet)
-
-                    # Set Tile
-                    b.set_tile(counter - 1, i, glet, 'green')
-                    
-                    # update options
-                    b.update_choices(i, glet, 'green')
-            
-                # if letter in target but incorrect position
-                if i != j and glet is TARGET[j]:
-                    print(f'letter match, position incorrect')
-                    
-                    # If glet is in green, already know where the correct position is 
-                    if glet not in b.greens[i]:
-                        
-                        
-                        # If already in yellow, no need to update
-                        if glet not in b.yellows[i]:
-                            
-                            # color yellow
-                            b.yellows[i].append(glet)
-                            
-                            # Update options
-                            b.update_choices(i, glet, 'yellow')
-                        
-                        # create Tile for board
-                        b.set_tile(counter - 1, i, glet, 'yellow')
-                            
-
-
-            # if letter not within word
-            print(f'Checking if grey by default')
-            if glet not in b.greens[i]:
-                print(f'not within greens in position {i}')
-                if glet not in b.yellows[i]:
-                    print(f'not within yellows in position {i}')
-
-                    # color grey
-                    b.greys.append(glet)
-        
-                    # set Tile
-                    b.set_tile(counter - 1, i, glet, 'grey')
-                    
-                    # update options
-                    b.update_choices(i, glet, 'grey')
-                
         # display options
         b.print_choices() 
         
@@ -120,7 +60,7 @@ def main():
 
         # if the guess is the same as target word
         if guess == TARGET:
-            print(f'You Win!')
+            print(f'You Win!\nFound target in {counter} guesses')
             correct_guess = True
 
         # if reached max guesses
